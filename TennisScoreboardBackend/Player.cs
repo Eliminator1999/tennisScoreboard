da498566eb7bfa6ffba6c264aadd302f4c9074b0
eliminator1999 <elientelis1999@gmail.com>
2020-12-19 13:13:11 +0200

    created class Player

--- TennisScoreboardBackend/Player.cs
+++ TennisScoreboardBackend/Player.cs
@@ -2,7 +2,15 @@
 
 namespace TennisScoreboardBackend
 {
-    public class Class1
+    public class Player
     {
+        private static readonly Random rand = new Random();
+        public string name { get; }
+        public uint id { get; }
+        public Player(string name)
+        {
+            this.name = name;
+            this.id = (uint)rand.Next();
+        }
     }
 }
