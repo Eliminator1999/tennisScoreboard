919dd8b0e3a8ad301fe83cf9c13d5ae4c738c2e3
eliminator1999 <elientelis1999@gmail.com>
2020-12-19 13:18:24 +0200

    added Game class template and GameunitTests

--- TennisScoreboardBackend/Game.cs
+++ TennisScoreboardBackend/Game.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace TennisScoreboardBackend
 {
-    class Class1
+    public class Game
     {
+        private IDictionary<uint, string> gameScore { get; set; }
+        public Game(Player firstPlayer, Player secondPlayer)
+        {
+            this.gameScore = new Dictionary<uint, string>
+            {
+                { firstPlayer.id, "0" },
+                { secondPlayer.id, "0" }
+            };
+        }
+
+        public string AddScore(Player player)
+        {
+            return "";
+        }
+
+        public void ResetScore()
+        {
+
+        }
     }
 }
--- TennisScoreboardUnitTests/GameUnitTests.cs
+++ TennisScoreboardUnitTests/GameUnitTests.cs
@@ -1,13 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisScoreboardBackend;
 
 namespace TennisScoreboardUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class GameUnitTests
     {
+        private Player firstPlayer = new Player("eli");
+        private Player secondPlayer = new Player("yael");
+
         [TestMethod]
-        public void TestMethod1()
+        public void AddScoreLinearTest()
         {
+            Game game = new Game(firstPlayer, secondPlayer);
+            Assert.AreEqual("15", game.AddScore(firstPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("30", game.AddScore(firstPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("40", game.AddScore(firstPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("win", game.AddScore(firstPlayer), false, "Score was not calculated correctly");
+
+        }
+        public void AddScoreDeuceAdvantageTest()
+        {
+            Game game = new Game(firstPlayer, secondPlayer);
+            for (int i = 0; i < 3; i++)
+            {
+                game.AddScore(firstPlayer);
+                game.AddScore(secondPlayer);
+            }
+            Assert.AreEqual("advantage", game.AddScore(firstPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("deuce", game.AddScore(secondPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("advantage", game.AddScore(secondPlayer), false, "Score was not calculated correctly");
+            Assert.AreEqual("win", game.AddScore(secondPlayer), false, "Score was not calculated correctly");
+
+        }
+        public void ResetScoreUnitTest()
+        {
+            Game game = new Game(firstPlayer, secondPlayer);
+            game.AddScore(firstPlayer);
+            game.AddScore(secondPlayer);
+            game.ResetScore();
+            Assert.AreEqual("15", game.AddScore(firstPlayer), false, "Reset score did not return zero");
+            Assert.AreEqual("15", game.AddScore(secondPlayer), false, "Reset score did not return zero");
         }
     }
 }
