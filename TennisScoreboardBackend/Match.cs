60298b8cff5301e31ea9a89106878c48bd14067e
eliminator1999 <elientelis1999@gmail.com>
2020-12-20 18:12:37 +0200

    class Match added with all tests suscessfull

--- TennisScoreboardBackend/Match.cs
+++ TennisScoreboardBackend/Match.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace TennisScoreboardBackend
 {
-    class Match
+    public class Match
     {
+        private Set set;
+        private IDictionary<uint, uint> setScore { get; set; }
+        public Match(Player firstPlayer, Player secondPlayer)
+        {
+            this.set = new Set(firstPlayer, secondPlayer);
+            this.setScore = new Dictionary<uint, uint>
+            {
+                { firstPlayer.id, 0},
+                { secondPlayer.id, 0}
+            };
+        }
+        public uint AddScore(Player player)
+        {
+            string pointScore = this.set.AddScore(player);
+            if (pointScore == "win")
+            {
+                setScore[player.id]++;
+            }
+            return setScore[player.id];
+        }
     }
 }
---
+++ TennisScoreboardUnitTests/MatchUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisScoreboardBackend;
+
+namespace TennisScoreboardUnitTests
+{
+    [TestClass]
+    public class MatchUnitTests
+    {
+        private Player firstPlayer = new Player("eli");
+        private Player secondPlayer = new Player("yael");
+        [TestMethod]
+        public void AddScoreLinearTest()
+        {
+            Match match = new Match(firstPlayer, secondPlayer);
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    match.AddScore(firstPlayer);
+                }
+            }
+            Assert.AreEqual((uint) 1, match.AddScore(firstPlayer));
+        }
+    }
+}
